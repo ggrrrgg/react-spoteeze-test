5e57042eb20dba8671b68fe35d8e5d18a88cf2c4
ggrrrgg <george.sheridan@gmail.com>
2023-09-21 17:41:10 +1000

    researching spotify api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
+import React from 'react';
 import './App.css';
 
+// user to retrieve collection from spotify api then select a release to return album credits from deezer
 function App() {
   return (
     <div className="App">
-      {/* user input artist and album name to retrieve available credits info from deezer api */}
-      <h1>Freshmess</h1>
+      
+      <h1>Albumn Credits for Spotify Collection</h1>
       <form id="userInput" >
       
-        <h2>Please enter artist and release title:</h2>
+        <h2>Researching..</h2>
         <input type="text" value="" id="input"></input>
       <button onClick="search()">Go!</button>
       </form>
