ce69ec59ce9980d31c8b0aecbb407189a56b42ff
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-09-27 14:18:41 +1000

    spotify search returning

--- src/App.js
+++ src/App.js
@@ -1,18 +1,91 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+
+const CLIENT_ID = "88cf4f8c14764902be12a88d1c6271ab";
+const CLIENT_SECRET = "e1531ab8db3f482ab4926524ab8e4b55";
 
 // user to retrieve collection from spotify api then select a release to return album credits from deezer
 function App() {
+  const [searchInput, setSearchInput] = useState('');
+  const [accessToken, setAccessToken] = useState('');
+
+
+
+  useEffect(() => {
+    let authParameters = {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
+
+    }
+    fetch('https://accounts.spotify.com/api/token', authParameters)
+      .then(result => result.json())
+      .then(data => setAccessToken(data.access_token))
+  }, []);
+
+  // Search
+  async function search() {
+    console.log(`Searching for ${searchInput}`);
+
+    // Get request to get the Artist ID
+    let artistParameters = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+      }
+    }
+
+    let artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', artistParameters)
+    .then(response => response.json())
+    .then(data => { return data.artists.items[0].id})
+
+    console.log(`Artist ID is ${artistID}`);
+    // Get request with Artist ID to get all the albums or that artist
+    let albums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&limit=50', artistParameters)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    // Display those albums to the user
+  }
+
   return (
     <div className="App">
+      <Container>
+        <InputGroup className='mb-3' size='lg'>
+          <FormControl
+          placeholder='Search For Artist'
+          type='input'
+          onKeyPress={event => {
+            if (event.key == 'Enter') {
+              search();
+            }
+          }}
+          onChange={event => setSearchInput(event.target.value)}
+          />
+          <Button onClick={search}>
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className='mx-2 row row-cols-4'>
+          <Card>
+            <Card.Img src=''/>
+            <Card.Body>
+              <Card.Title>Album Name</Card.Title>
+            </Card.Body>
+          </Card>
+        </ Row>
+        
+      </Container>
       
-      <h1>Albumn Credits for Spotify Collection</h1>
-      <form id="userInput" >
-      
-        <h2>Researching..</h2>
-        <input type="text" value="" id="input"></input>
-      <button onClick="search()">Go!</button>
-      </form>
     </div>
   );
 }
